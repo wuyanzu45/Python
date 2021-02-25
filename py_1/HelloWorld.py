90908453db3a07c26760c583a8c2dce40ccd8146
wuyanzu45 <1169563282@qq.com>
2021-02-25 15:06:35 +0800

    Python 练习1

---
+++ py_1/HelloWorld.py
@@ -0,0 +1,14 @@
+import math
+# sentence = "  Hello World!  "
+# print(sentence[:])
+# print(sentence.strip())
+# print(sentence.rstrip())
+# print(sentence.lstrip())
+# print(sentence.replace("World", "Bit"))
+# a = "Hello" not in sentence
+# print(a)
+# print(math.ceil(2.1 + 3))
+# x = input("x: ")
+# y = int(x) + 1
+# print(f"y:,y:{y}")
+print("2")
