0674ee08fb21c373decbccb5ae73f4a1f1c8e3b3
wuyanzu45 <1169563282@qq.com>
2021-04-28 17:01:21 +0800

    _

---
+++ Codes/day07.py
@@ -0,0 +1,38 @@
+"""
+num = int("0b11", base=2)
+print(num)
+
+phone_namber = input("请输入手机号：")
+veri_code = input("请输入验证码：")
+phone_namber = phone_namber.strip()
+veri_code = veri_code.strip().upper()
+if phone_namber == "123" and veri_code == "2d3f".upper():
+    print("验证通过！")
+else:
+    print("验证码错误！")
+"""
+# print(True)
+# li = ["hhh", "xxx", "lll"]
+# print(li)
+# print(str(li))
+# num1 = bin(123)
+# num2 = bin(456)
+
+# text = input("请输入内容：")
+# text = text.replace("wuyanzu", "pengyuyan").replace("hhh", "xxx")
+# print(text)
+# print(text.replace())
+
+# msg = "伤情最是晚凉天，憔悴厮人不堪言"
+# for i in range(len(msg)):
+#     print(msg[i])
+# text = input("请输入内容：")
+# num_text = ""
+# for item in text:
+#     if item.isdecimal():
+#         num_text = num_text + item
+# print(num_text)
+# n = 1
+# n = 3
+# print(n)
+
